b08c53da1bbe16bc2bb66f4826bd620a676fd6ca
Tim Torndal <tim.torndal@skola.botkyrka.se>
2020-10-19 15:11:46 +0200

    Solve 6

---
+++ 6/script.js
@@ -0,0 +1,13 @@
+function isSameUser(tim, hampus){
+ return tim.mail == hampus.mail
+}
+
+const tim = {
+    mail: 'flupp@mail.com'
+}
+
+const hampus = {
+    mail: 'flupp@mail.com'
+}
+
+console.log(isSameUser)
